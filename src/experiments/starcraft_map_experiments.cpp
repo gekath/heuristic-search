643c7f6a58eab84a14c14d8e35e4809082f6ce8d
gekath <kathyge@gmail.com>
2016-10-16 12:52:50 -0400

    Calculate average, median

--- src/experiments/starcraft_map_experiments.cpp
+++ src/experiments/starcraft_map_experiments.cpp
@@ -23,6 +23,40 @@
 
 using namespace std;
 
+double compute_median(vector<int> scores);
+double compute_average(vector<int> scores);
+
+double compute_median(vector<int> scores) {
+
+    double median;
+    size_t size = scores.size();
+    sort(scores.begin(), scores.end());
+
+    if (size % 2 == 0) {
+        median = (scores[size / 2 - 1] + scores[size / 2]) / 2;
+    } else {
+        median = scores[size / 2];
+    }
+
+    return median;
+
+}
+
+double compute_average(vector<int> scores) {
+
+    int sum = 0 ;
+    int i;
+    size_t size = scores.size();
+
+    for (i = 0; i < size; i ++) {
+        sum = sum + scores[i];
+    }
+
+    return sum / size;
+
+}
+
+
 int main(int argc, char **argv)
 {
     AStar<MapLocation, MapDir> a_star;
