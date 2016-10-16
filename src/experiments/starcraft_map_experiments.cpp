34e1c80c8604d7770f26ccc58364027b332de1d4
gekath <kathyge@gmail.com>
2016-10-16 14:41:41 -0400

    Calculate percentage

--- src/experiments/starcraft_map_experiments.cpp
+++ src/experiments/starcraft_map_experiments.cpp
@@ -182,9 +182,9 @@ int main(int argc, char **argv)
     cout << default_avg << "\t" << low_g_avg << "\t" << high_g_avg << endl;
 
     unsigned int i;
-    double default_count;
-    double low_g_count;
-    double high_g_count;
+    double default_count = 0;
+    double low_g_count = 0;
+    double high_g_count = 0;
     for (i = 0; i < default_nodes.size(); i++) {
 
         int min_val = min({default_nodes[i], low_g_nodes[i], high_g_nodes[i]});
