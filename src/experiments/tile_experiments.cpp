8d29d1f7304017eb2bf84fd679c9c0126b8bf03f
gekath <kathyge@gmail.com>
2016-10-17 01:26:09 -0400

    Get most costly problem

--- src/experiments/tile_experiments.cpp
+++ src/experiments/tile_experiments.cpp
@@ -167,11 +167,6 @@ int main(int argc, char **argv)
     // vector<int> gbfs_nodes(starts.size());
     // vector<int> gbfs_cost(starts.size());
 
-    cout << "here" << endl;
-
-    unsigned int highest_cost_idx = 0;
-    int highest_cost = -1;
-
     for(unsigned i = 0; i < starts.size(); i++) {
         TilePuzzleState start_state(starts[i], 3, 4);
 
@@ -184,16 +179,8 @@ int main(int argc, char **argv)
         int cur_cost = a_1.getLastPlanCost();
         a1_cost[i] = cur_cost;
         // gbfs_cost[i] = gbfs.getLastPlanCost();
-
-        if (cur_cost > highest_cost) {
-            highest_cost_idx = i;
-            highest_cost = cur_cost;
-        }
-
     }
 
-    cout << "Problem: " << highest_cost_idx << endl;
-    
 
     double a1_median_nodes = compute_median(a1_nodes);
     // double gbfs_median_nodes = compute_median(gbfs_nodes);
@@ -221,6 +208,10 @@ int main(int argc, char **argv)
     // vector<int> gbfs_nodes_low(starts_low.size());
     // vector<int> gbfs_cost_low(starts_low.size());
 
+    unsigned int highest_cost_idx = 0;
+    int highest_cost = -1;
+
+
     for(unsigned i = 0; i < starts_low.size(); i++) {
         TilePuzzleState start_state_low(starts_low[i], 3, 4);
 
@@ -234,9 +225,14 @@ int main(int argc, char **argv)
         a1_cost_low[i] = cur_cost;
         // gbfs_cost_low[i] = gbfs_low.getLastPlanCost();
 
-
+        if (cur_cost > highest_cost) {
+            highest_cost_idx = i;
+            highest_cost = cur_cost;
+        }
 
     }
+
+    cout << "Problem: " << highest_cost_idx << endl;
 
 
     double a1_median_nodes_low = compute_median(a1_nodes_low);
